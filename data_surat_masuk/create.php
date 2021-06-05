7ccd53db83495f81aaefbb1a0ecfe51c6a9ccc25
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:35:02 +0700

    api data_surat _masuk

--- data_surat_masuk/create.php
+++ data_surat_masuk/create.php
@@ -4,13 +4,12 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_surat_masuk = $data['id_surat_masuk'];
 $id_jenis_surat = $data['id_jenis_surat'];
 $tgl_masuk = $data['tgl_masuk'];
 $perihal = $data['perihal'];
 
-if (!mysqli_query($conn, "INSERT into user (id_surat_masuk, id_jenis_surat, tgl_masuk, perihal) 
-                                    values ('$id_surat_masuk', '$id_jenis_surat', '$tgl_masuk', '$perihal')")){
+if (!mysqli_query($conn, "INSERT into user ( id_jenis_surat, tgl_masuk, perihal) 
+                                    values ( '$id_jenis_surat', '$tgl_masuk', '$perihal')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
