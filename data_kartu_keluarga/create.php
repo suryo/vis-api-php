6e885c8f3fb6c0d176dd07fca7578f53feaadda4
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:26:21 +0700

    api data_kartu_keluarga

--- data_kartu_keluarga/create.php
+++ data_kartu_keluarga/create.php
@@ -4,12 +4,11 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_data = $data['id_data'];
 $no_kk = $data['no_kk'];
 $nik = $data['nik'];
 
-if (!mysqli_query($conn, "INSERT into user (id_data, no_kk, nik) 
-                                    values ('$id_data', '$no_kk', '$nik')")){
+if (!mysqli_query($conn, "INSERT into user (no_kk, nik) 
+                                    values ('$no_kk', '$nik')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
