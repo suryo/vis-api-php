6994bb55079da99aac3b83d48c9deaf7d1115cf6
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:25:12 +0700

    api data_potensi_desa

--- data_jenis_potensi_desa/create.php
+++ data_jenis_potensi_desa/create.php
@@ -4,12 +4,9 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_potensi = $data['id_potensi'];
 $nama_potensi = $data['nama_potensi'];
 
-
-
-if (!mysqli_query($conn, "INSERT into user (id_potensi, nama_potensi) values ('$id_potensi', '$nama_potensi')")){
+if (!mysqli_query($conn, "INSERT into user (nama_potensi) values ('$nama_potensi')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
