b43036ef84da6118ab1f3cd86dcbd94f55c066fe
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:29:26 +0700

    api data_perangkat_desa

--- data_perangkat_desa/create.php
+++ data_perangkat_desa/create.php
@@ -4,12 +4,11 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_perangkat_desa = $data['id_perangkat_desa'];
 $nama_perangkat_desa = $data['nama_perangkat_desa'];
 $jabatan = $data['jabatan'];
 
-if (!mysqli_query($conn, "INSERT into user (id_perangkat_desa, nama_perangkat_desa, jabatan) 
-                                    values ('$id_perangkat_desa', '$nama_perangkat_desa', '$jabatan')")){
+if (!mysqli_query($conn, "INSERT into user ( nama_perangkat_desa, jabatan) 
+                                    values ( '$nama_perangkat_desa', '$jabatan')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
