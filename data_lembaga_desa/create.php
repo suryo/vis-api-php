9ee92a9f2392a9bfdfd68aecd7083db9837217f5
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:27:41 +0700

    api data_lembaga_desa

--- data_lembaga_desa/create.php
+++ data_lembaga_desa/create.php
@@ -4,12 +4,11 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_lembaga = $data['id_lembaga'];
 $nama_lembaga = $data['nama_lembaga'];
 $jenis_lembaga = $data['jenis_lembaga'];
 
-if (!mysqli_query($conn, "INSERT into user (id_lembaga, nama_lembaga, jenis_lembaga) 
-                                    values ('$id_lembaga', '$nama_lembaga', '$jenis_lembaga')")){
+if (!mysqli_query($conn, "INSERT into user ( nama_lembaga, jenis_lembaga) 
+                                    values ( '$nama_lembaga', '$jenis_lembaga')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
