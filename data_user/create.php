b561fb3d84743d0bb18283a3089a03c988f7fc68
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:35:47 +0700

    api data_user

--- data_user/create.php
+++ data_user/create.php
@@ -4,13 +4,12 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_user = $data['id_user'];
 $username = $data['username'];
 $password = $data['password'];
 $nik = $data['nik'];
 
-if (!mysqli_query($conn, "INSERT into user (id_user, username, password, nik) 
-                                    values ('$id_user', '$username', '$password', '$nik')")){
+if (!mysqli_query($conn, "INSERT into user (username, password, nik) 
+                                    values ( '$username', '$password', '$nik')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
