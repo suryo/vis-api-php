cf2132e339d043515aebb0600d64d97f012031fc
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:24:37 +0700

    fix data_jenis_lembaga_desa

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # vis-api-php
-untuk mencoba di local, anda bisa meng-akses utl berikut
-http://localhost/vis-api-php/read.php
+
+API with php programing
+
+1. Look at file koneksi.php
+2. set to your name database and user
+3. Try to open this link to check your database connection is true
+   http://localhost/vis-api-php/read.php
+
+example to access another API :
--- data_jenis_lembaga_desa/create.php
+++ data_jenis_lembaga_desa/create.php
@@ -4,12 +4,9 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_lembaga = $data['id_lembaga'];
 $nama_lembaga = $data['nama_lembaga'];
 
-
-
-if (!mysqli_query($conn, "INSERT into user (id_lembaga, nama_lembaga) values ('$id_lembaga', '$nama_lembaga')")){
+if (!mysqli_query($conn, "INSERT into user (nama_lembaga) values ('$nama_lembaga')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
