d17385f4eb7e501b1941f02c023df138001ffad9
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:34:24 +0700

    api data_surat_keluar

--- data_surat_keluar/create.php
+++ data_surat_keluar/create.php
@@ -4,13 +4,12 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$no_id_surat = $data['no_id_surat'];
 $id_surat_keluar = $data['id_surat_keluar'];
 $tgl_keluar = $data['tgl_keluar'];
 $perihal = $data['perihal'];
 
-if (!mysqli_query($conn, "INSERT into user (no_id_surat, id_surat_keluar, tgl_keluar, perihal) 
-                                    values ('$no_id_surat', '$id_surat_keluar', '$tgl_keluar', '$perihal')")){
+if (!mysqli_query($conn, "INSERT into user ( id_surat_keluar, tgl_keluar, perihal) 
+                                    values ( '$id_surat_keluar', '$tgl_keluar', '$perihal')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
