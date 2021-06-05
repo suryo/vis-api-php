23e46bd67d8b86578467645415ad428fd9560b55
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:28:18 +0700

    api data_master_surat

--- data_master_surat/create.php
+++ data_master_surat/create.php
@@ -4,7 +4,6 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_surat = $data['id_surat'];
 $no_surat = $data['no_surat'];
 $keterangan_surat = $data['keterangan_surat'];
 $kepada = $data['kepada'];
@@ -13,8 +12,8 @@ $tanggal = $data['tanggal'];
 $tempat = $data['tempat'];
 $kepala_desa = $data['kepala_desa'];
 
-if (!mysqli_query($conn, "INSERT into user (id_surat, no_surat, keterangan_surat, kepada, alamat, tanggal, tempat, kepala_desa) 
-                                    values ('$id_surat', '$no_surat', '$keterangan_surat', '$kepada', '$alamat', '$tanggal', '$tempat', '$kepala_desa')")){
+if (!mysqli_query($conn, "INSERT into user ( no_surat, keterangan_surat, kepada, alamat, tanggal, tempat, kepala_desa) 
+                                    values ('$no_surat', '$keterangan_surat', '$kepada', '$alamat', '$tanggal', '$tempat', '$kepala_desa')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
