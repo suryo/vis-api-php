1d5575ca23b9f398cbe15e06f35ca808be04212d
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-05 09:28:48 +0700

    api data_penduduk

--- data_penduduk/create.php
+++ data_penduduk/create.php
@@ -4,15 +4,14 @@ include('../koneksi.php');
 $json = file_get_contents('php://input');
 $data = json_decode($json,true);
 
-$id_penduduk = $data['id_penduduk'];
 $nik = $data['nik'];
 $no_kk = $data['no_kk'];
 $nama_penduduk = $data['nama_penduduk'];
 $jenis_kelamin = $data['jenis_kelamin'];
 $alamat_penduduk = $data['alamat_penduduk'];
 
-if (!mysqli_query($conn, "INSERT into user (id_penduduk, nik, no_kk, nama_penduduk, jenis_kelamin, alamat_penduduk) 
-                                    values ('$id_penduduk', '$nik', '$no_kk', '$nama_penduduk', '$jenis_kelamin', '$alamat_penduduk')")){
+if (!mysqli_query($conn, "INSERT into user ( nik, no_kk, nama_penduduk, jenis_kelamin, alamat_penduduk) 
+                                    values ( '$nik', '$no_kk', '$nama_penduduk', '$jenis_kelamin', '$alamat_penduduk')")){
     $status = array(
         'status' => "Error: %s\n", $conn->error
     );
